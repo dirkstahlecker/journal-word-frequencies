adc4e77c9b53d79510f861b317104836c8271251
Dirk Stahlecker <dirkstahlecker@Dirks-MacBook-Pro.local>
2020-12-29 12:55:09 -0700

    added tabs back in

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import {observer} from "mobx-react";
 import {makeObservable, observable, runInAction} from "mobx";
 import { JournalReader, JournalReaderMachine } from './JournalReader';
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import 'react-tabs/style/react-tabs.css';
 
 export class AppMachine
 {
@@ -42,7 +44,68 @@ class App extends React.Component<AppProps>
   render()
   {
     return <div className="App">
-      <JournalReader machine={this.machine.journalReaderMachine}/>
+      <span style={{width: "100%", height: "100%", display: "inline-block", verticalAlign: "top"}}
+            tabIndex={0}
+            id="mainApp"
+      >
+        <Tabs>
+          <TabList>
+            <Tab>
+              Write
+            </Tab>
+            <Tab>
+              Read
+            </Tab>
+            <Tab>
+              Add Markup
+            </Tab>
+            <Tab>
+              Stats
+            </Tab>
+            <Tab>
+              Graph Testing
+            </Tab>
+          </TabList>
+
+          <TabPanel>
+            {/* <JournalWriter machine={this.props.machine.journalWriterMachine}/> */}
+          </TabPanel>
+          <TabPanel>
+            <JournalReader machine={this.machine.journalReaderMachine}/>
+          </TabPanel>
+          <TabPanel>
+            <div onKeyDown={(e: any) => {
+              if (e.key === "Enter")
+              {
+                e.preventDefault();
+                // this.machine.handleEasyMarkupGeneratorSubmit();
+              }
+            }}>
+              First Name: <input type="text" id="firstName" autoFocus={true}/>
+              <br />
+              Last Name: <input type="text" id="lastName" />
+              <br />
+              Display Name: <input type="text" id="displayName" />
+              <br />
+              {/* <button onClick={this.machine.handleEasyMarkupGeneratorSubmit}>Submit</button> */}
+              <br />
+              <br />
+              <div id="displayCopyArea">
+                <input id="placeToSelectText" />
+              </div>
+            </div>
+          </TabPanel>
+          <TabPanel>
+            {/* <Stats
+              machine={new StatsMachine("TODO")}
+            /> */}
+          </TabPanel>
+          <TabPanel>
+
+          </TabPanel>
+        </Tabs>
+
+      </span>
     </div>
   }
 }
