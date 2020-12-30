87803783e2dbfb0d783735040738b6fd2f202a22
Dirk Stahlecker <dirkstahlecker@Dirks-MacBook-Pro.local>
2020-12-30 10:37:02 -0700

    changed database connection credentials for heroku

---
+++ src/init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE names (
+  displayname text,
+  firstname text[],
+  lastname text[]
+);
+
+
+-- INSERT INTO names (displayname, firstname, lastname)
+--   VALUES ('dirk', '{dirk}', '{stahlecker}'), ('hammer', '{arthur}', '{hammer}');
--- src/queries.ts
+++ src/queries.ts
@@ -1,10 +1,11 @@
 import { Pool } from 'pg'
 
 const pool = new Pool({
-  user: 'dirkstahlecker',
-  host: 'localhost',
-  database: 'wordfreq',
+  user: 'tsbqunitsyqmur', //'dirkstahlecker',
+  host: process.env.DATABASE_URL || 'localhost',
+  database: 'dfd3r33aio9vo3', //'wordfreq',
   // password: 'password',
+  password: '5fdc0e7b6c8978b6dbf9291025a3e510f49ca1fef9436e8917cffdd167b800ac',
   port: 5432,
 })
 
